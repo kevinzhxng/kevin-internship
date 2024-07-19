7bea49acddc8415828725a35aa1fed6074351b19
kevinzhxng <kzhang0826@gmail.com>
2024-07-19 18:30:12 -0400

    finished author section

--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -5,6 +5,7 @@ import { Link, useParams } from "react-router-dom";
 import AuthorImage from "../images/author_thumbnail.jpg";
 import axios from "axios";
 import Followers from "../components/UI/Followers";
+import Skeleton from "../components/UI/Skeleton";
 
 const Author = () => {
   const { id } = useParams();
@@ -39,38 +40,108 @@ const Author = () => {
         <section aria-label="section">
           <div className="container">
             <div className="row">
-              <div className="col-md-12">
-                <div className="d_profile de-flex">
-                  <div className="de-flex-col">
-                    <div className="profile_avatar">
-                      <img src={data.authorImage} alt="" />
+              {loading ? (
+                <>
+                  <div className="col-md-12">
+                    <div className="d_profile de-flex">
+                      <div className="de-flex-col">
+                        <div className="profile_avatar">
+                          <Skeleton
+                            width={150}
+                            height={150}
+                            borderRadius={99}
+                          />
+                          <i className="fa fa-check"></i>
+                          <div className="profile_name">
+                            <h4>
+                              <Skeleton width={200} height={30} />
+                              <span className="profile_username">
+                                <Skeleton width={100} height={20} />
+                              </span>
+                              <span id="wallet" className="profile_wallet">
+                                <Skeleton width={300} height={20} />
+                              </span>
+                            </h4>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="profile_follow de-flex">
+                        <div className="de-flex-col">
+                          <div className="profile_follower">
+                            <Skeleton width={120} height={25} />
+                          </div>
 
-                      <i className="fa fa-check"></i>
-                      <div className="profile_name">
-                        <h4>
-                          {data.authorName}
-                          <span className="profile_username">@{data.tag}</span>
-                          <span id="wallet" className="profile_wallet">
-                            {data.address}
-                          </span>
-                          <button id="btn_copy" title="Copy Text">
-                            Copy
-                          </button>
-                        </h4>
+                          <Skeleton width={125} height={45} borderRadius={8} />
+                        </div>
                       </div>
                     </div>
                   </div>
-                  <div className="profile_follow de-flex">
-                    {data.followers ? <Followers followers={data.followers} /> : null}
-                  </div>
-                </div>
-              </div>
 
-              <div className="col-md-12">
-                <div className="de_tab tab_simple">
-                  <AuthorItems data={data.nftCollection} authorPic={data.authorImage}/>
-                </div>
-              </div>
+                  <div className="col-md-12">
+                    <div className="de_tab tab_simple">
+                      <div className="de_tab_content">
+                        <div className="tab-1">
+                          <div className="row">
+                            {data
+                              ? new Array(8).fill(0).map((_, index) => (
+                                  <div
+                                    className="col-lg-3 col-md-6 col-sm-6 col-xs-12 pb-3"
+                                    key={index}
+                                  >
+                                    <Skeleton
+                                      width="100%"
+                                      height={440}
+                                      borderRadius={20}
+                                    />
+                                  </div>
+                                ))
+                              : null}
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </>
+              ) : (
+                <>
+                  <div className="col-md-12">
+                    <div className="d_profile de-flex">
+                      <div className="de-flex-col">
+                        <div className="profile_avatar">
+                          <img src={data.authorImage} alt="" />
+
+                          <i className="fa fa-check"></i>
+                          <div className="profile_name">
+                            <h4>
+                              {data.authorName}
+                              <span className="profile_username">
+                                @{data.tag}
+                              </span>
+                              <span id="wallet" className="profile_wallet">
+                                {data.address}
+                              </span>
+                              <button id="btn_copy" title="Copy Text">
+                                Copy
+                              </button>
+                            </h4>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="profile_follow de-flex">
+                        {data.followers ? (
+                          <Followers followers={data.followers} />
+                        ) : null}
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="col-md-12">
+                    <div className="de_tab tab_simple">
+                      <AuthorItems data={data} authorPic={data.authorImage} />
+                    </div>
+                  </div>
+                </>
+              )}
             </div>
           </div>
         </section>
